625a910ec4f8db6448d2d8dca7102f80605c6871
GornushkinAP <gapEdu@yandex.ru>
2023-07-12 22:50:14 +0300

    Сделал задачу 27

---
+++ Task27/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// // 452 -> 11
+// // 82 -> 10
+// // 9012 -> 12
+
+Console.Write("Здравствуйте. Введите целое число: ");
+string input = Console.ReadLine(); 
+
+int number;
+if (int.TryParse(input, out number)) 
+{
+    int sum = CalculateDigitSum(number); 
+
+    Console.WriteLine("Сумма цифр в числе: " + sum);
+    Console.WriteLine("Досвидания.");
+}
+else
+{
+    Console.WriteLine("Ошибка! Введите корректное число.");
+}
+
+
+static int CalculateDigitSum(int number)
+{
+    int sum = 0;
+
+    while (number != 0)
+    {
+        int digit = number % 10; 
+        sum += digit; 
+        number /= 10; 
+    }
+
+    return sum;
+}
