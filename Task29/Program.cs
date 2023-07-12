84b0e93acf9b8ae51698b2838ebc5fb069160481
GornushkinAP <gapEdu@yandex.ru>
2023-07-13 00:01:11 +0300

    Сделал задачу 29

---
+++ Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов в диапазоне от 10 до 1000 и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int randomNumber = new Random().Next(10, 100);
+
+int[] numbers = new int[8];
+
+
+Random randomArray = new Random();
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = randomArray.Next(10, 1001);
+}
+
+
+Console.WriteLine("Элементы массива: ");
+Console.Write("[");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
+Console.Write("]");
+Console.WriteLine();
